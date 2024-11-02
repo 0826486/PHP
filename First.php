72eca782b76bcc8587ae02ae684312c970f3a134
호정 <w2314@e-mirim.hs.kr>
2024-11-02 23:29:05 +0900

    1. php 기초 문법

---
+++ First.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php 연습</title>
+</head>
+<body>
+    <h1>PHP 연습</h1>
+
+    <?php
+    echo "안녕하세요";  // echo에 ; 필수
+
+    // 변수 선언 ($)
+    $a = 1;
+    ?>
+</body>
+</html>
